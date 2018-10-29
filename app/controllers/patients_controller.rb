b9bef008122a023255b015ab39d40869fc959e82
little_wings <little_wings@foxmail.com>
2018-10-29 13:53:37 +0800

    patient index page

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,16 +1,29 @@
 class PatientsController < ApplicationController
-  layout 'application', only: [:index,:all_patients,:under_research,:under_followup,:quit_followup,:quited]
+  layout 'application', only: [:index,:all_patients,:under_research,:under_followup,:quit_followup,:quited,
+                               :followup_queue,:research_queue]
   before_action :authenticate_user!
   before_action :authenticate_project
   load_and_authorize_resource :patient
   before_action :load_available_centers,only:[:new,:edit]
 
   def followup_queue
-
+    @patients=Patient.joins(:followup_monitor).accessible_by(current_ability,:read).where(status:2).where(
+        'overdue_followup>0').order(:created_at).page(params[:page])
+    @patients.each{|patient|
+      patient.set_center_name
+      patient.set_overdue_courses
+      patient.set_overdue_followups
+    }
   end
 
   def research_queue
-
+    @patients=Patient.joins(:course_monitor).accessible_by(current_ability,:read).where(status:1).where(
+    'overdue_course>0').order(:created_at).page(params[:page])
+    @patients.each{|patient|
+      patient.set_center_name
+      patient.set_overdue_courses
+      patient.set_overdue_followups
+    }
   end
 
   def all_patients
@@ -74,24 +87,17 @@ class PatientsController < ApplicationController
   end
 
   def index
-    @project_total_planned_patients=0
-    current_project.center_project_relationships.all.each{|cpr|
-      @project_total_planned_patients+=cpr.planned_patients_count
-    }
-    @project_total_patients=Patient.where(project_id:current_project.id).size()
 
+    get_center_planned_patients
 
-    @center_planned_patients={}
-    current_user.relationships.where(project_id:current_project.id).each{|r|
+    get_project_total_planned_patients
 
-      inquire=CenterProjectRelationship.find_by(project_id: current_project.id,
-                                        center_id:r.center_id)
-      if inquire.nil?
-        @center_planned_patients[r.center.name]=0
-      else
-        @center_planned_patients[r.center.name]=inquire.planned_patients_count
-      end
-    }
+    get_center_patients
+
+    @followup_queue_size=get_followup_queue_size
+
+    @research_queue_size=get_research_queue_size
+
   end
 
   def update
@@ -128,15 +134,42 @@ class PatientsController < ApplicationController
     @available_centers=Center.find(centers_list)
   end
 
-  # def load_patients
-  #   @patients=Patient.accessible_by current_ability,:read
-  #   @patients = @patients.order(:created_at).page(params[:page])
-  #
-  #   @patients.each{|patient|
-  #     patient.set_center_name
-  #     patient.set_overdue_courses
-  #     patient.set_overdue_followups
-  #   }
-  # end
+  def get_center_planned_patients
+    @center_planned_patients={}
+    current_user.relationships.where(project_id:current_project.id).each{|r|
 
+      inquire=CenterProjectRelationship.find_by(project_id: current_project.id,
+                                                center_id:r.center_id)
+      if inquire.nil?
+        @center_planned_patients[r.center.name]=0
+      else
+        @center_planned_patients[r.center.name]=inquire.planned_patients_count
+      end
+    }
+  end
+
+  def get_project_total_planned_patients
+    @project_total_planned_patients=0
+    current_project.center_project_relationships.all.each{|cpr|
+      @project_total_planned_patients+=cpr.planned_patients_count
+    }
+    @project_total_patients=Patient.where(project_id:current_project.id).size()
+  end
+
+  def get_center_patients
+    @center_patients={}
+    current_user.relationships.where(project_id:current_project.id).each{|r|
+      @center_patients[r.center.name]=Patient.where(project_id:current_project.id,center_id:r.center_id).size
+    }
+  end
+
+  def get_followup_queue_size
+    @patients=Patient.joins(:followup_monitor).accessible_by(current_ability,:read).where(status:2).where(
+        'overdue_followup>0').order(:created_at).page(params[:page]).size
+  end
+
+  def get_research_queue_size
+    @patients=Patient.joins(:course_monitor).accessible_by(current_ability,:read).where(status:1).where(
+        'overdue_course>0').order(:created_at).page(params[:page]).size
+  end
 end
