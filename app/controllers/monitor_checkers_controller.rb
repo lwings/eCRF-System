486a0a303e1b0cf9b623abe09b021aa14a157039
little_wings <little_wings@foxmail.com>
2018-11-17 10:53:43 +0800

    fix ability in infringemwnt detection

--- app/controllers/monitor_checkers_controller.rb
+++ app/controllers/monitor_checkers_controller.rb
@@ -1,8 +1,11 @@
 class MonitorCheckersController < ApplicationController
   before_action :authenticate_user!
-  # authorize_resource
   def index
-    file_path=Rails.configuration.monitor_log['path']
-    @arr=IO.readlines(file_path)
+    if can? :read, MonitorCheckersController
+      file_path=Rails.configuration.monitor_log['path']
+      @arr=IO.readlines(file_path)
+    else
+      redirect_to '/infringement'
+    end
   end
 end
