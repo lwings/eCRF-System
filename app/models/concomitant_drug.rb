35ca4a599df6e85543359f435a602daaae979ab6
little_wings <little_wings@foxmail.com>
2019-04-06 20:48:40 +0800

    add something to patient

--- app/models/concomitant_drug.rb
+++ app/models/concomitant_drug.rb
@@ -3,5 +3,5 @@ class ConcomitantDrug < ActiveRecord::Base
   belongs_to :patient
 
   validates :drugs,presence: true
-  validates :daily_dose,presence: true
+  #validates :daily_dose,presence: true
 end
